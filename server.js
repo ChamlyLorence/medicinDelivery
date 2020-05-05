3247f14876c9946b6b4e0d03b050eb46ace23c70
Chamly <chamlylorence@gmail.com>
2020-05-05 06:28:35 +0530

    fore commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGOOB_URI || "mongodb://127.0.0.1:27017/chamara" ;
+const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/chamara" ;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -22,8 +23,16 @@ const usersRouter = require('./routes/users');
 app.use('/orders', ordersRouter);
 app.use('/users', usersRouter);
 
+// set NODE_ENV=production
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
+    app.get('*',(req,res) =>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+      
+    } );
+    console.log("ok");
 }
 
 app.listen(port, () => {
